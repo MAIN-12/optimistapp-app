7de09d712e4e2c54e238db0eb394395163880e24
CHARLIE <jcarlosbotero1@outlook.com>
2025-02-11 15:58:53 -0500

    update logo

---
+++ src/config/logo/AppLogoCompact .tsx
@@ -0,0 +1,10 @@
+const AppLogoCompact = () => {
+    return <svg
+        viewBox="0 0 7.938 7.937"
+        height="30" width="30"
+        xmlns="http://www.w3.org/2000/svg">
+        <g><path className="fill-black dark:fill-white" d="M670.52-717.512h-3.961v3.961m-3.961-3.961h3.96v-3.96" transform="translate(-663.961 722.955)scale(1.00206)" /></g>
+    </svg>
+}
+
+export default AppLogoCompact;
